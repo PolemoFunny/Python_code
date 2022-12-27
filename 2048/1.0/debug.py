c4fb206bfacb05f6f5d55faab7b22dd8289a2aba
柚子滑稽 <108924722+PolemoFunny@users.noreply.github.com>
2022-12-27 09:10:54 +0800

    Update debug.py
    小改了一下

--- 2048/1.0/debug.py
+++ 2048/1.0/debug.py
@@ -2,44 +2,43 @@
 #Python
 
 #变量
-xy=[0,1,2,3,\
-    0,0,0,0,\
-    0,0,0,0,\
-    0,0,0,0]
+xy = [0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 return_direction=False
-temp_xy=False
+operational_parameters = False
 
 def mobile(direction):
     global xy
-#//////////ERROR//////////#
-    if direction =='a'or'w':
-        temp_xy=15
-    if direction =='s'or'd':
-        temp_xy=0
-#//////////ERROR//////////#
-    print('temp_xy='+str(temp_xy))
-    print('direction='+direction)
-    if temp_xy != False:
+    if (direction in ['a','w']):
+        operational_parameters = [15, -1]
+    if (direction in ['s', 'd']):
+        operational_parameters = [0, 1]
+    print(('operational_parameters=' + str(operational_parameters)))
+    print(('direction=' + direction))
+    if operational_parameters != False:
         print('Ln:21 True')
-        if xy[temp_xy] == 0:
+        if (xy[operational_parameters[0]+operational_parameters[1]] == 0):
             print('Ln:23 True')
-            if xy[temp_xy+1] == 0:
+            if (xy[operational_parameters[0]+operational_parameters[1]] == 0):
                 print('Ln:25 True')
-                if xy[temp_xy+2] == 0:
-                    xy[temp_xy+2]=xy[temp_xy+3]
-                    xy[temp_xy+3]=0
-            else:
+                if (xy[(operational_parameters[0]+operational_parameters[1] * 2)] == 0):
+                    xy[operational_parameters[0]+operational_parameters[1]*2]=xy[operational_parameters[1]*3]
+                    xy[operational_parameters[0]+operational_parameters[1]*3]=0
+            else :
                 print('Ln:25 False')
-                xy[temp_xy+1]=xy[temp_xy+3]
-                xy[temp_xy+3]=0
-        else:
+                xy[operational_parameters[0]+operational_parameters[1]]=xy[operational_parameters[0]+operational_parameters[1]*3]
+                xy[operational_parameters[0]+operational_parameters[1]*3]=0
+        else :
             print('Ln:23 False')
-            print(xy[temp_xy])
-            xy[temp_xy]=xy[temp_xy+3]
-            xy[temp_xy+3]=0
-    else:
+            print(operational_parameters)
+            xy[operational_parameters[0]+operational_parameters[1]]=xy[operational_parameters[0]+operational_parameters[1]*3]
+            xy[operational_parameters[0]+operational_parameters[1]*3]=0
+    else :
         print('Ln:21 False')
+
 def merge():
+
     pass
+
 mobile('a')
-print('xy='+str(xy))
+print(('xy=' + str(xy)))
